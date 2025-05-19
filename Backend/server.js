5d83893ace14c7f6037298dfeb50ce0b410da726
Kanika <tomerkanika2002@gmail.com>
2025-05-19 11:14:23 +0530

    final push

--- Backend/server.js
+++ Backend/server.js
@@ -12,11 +12,7 @@ const app = express();
 
 //  Middleware to handle cors 
 app.use(
-  cors({
-    origin:process.env.CLIENT_URL ||"*",
-    methods:["GET","POST","PUT","DELETE"],
-    allowedHeaders:["Content-Type","Authorization"],
-  })
+  cors()
 );
 
 app.use(express.json());
